98bca6e9c9b475693c504f5e2fd30cfd6fa163dc
arepoGiovanna <nigelisidto@gmail.com>
2022-07-04 02:17:08 +0800

    Created index.php
    * Inserted simple index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <header>
+        <title>HMS</title>
+        <!-- insert icon tag here... -->
+    </header>
+    <body>
+        <h1>Hello World!</h1>
+        <h2>Hello World2!</h2>
+    </body>
+    <footer>
+        <!-- insert here... -->
+    </footer>
+</html>
