1d630465ee34c2c25863c7f86084c782bbf6cb42
Eliasaph Wan Uyo <eliwan3@gmail.com>
2022-07-04 02:59:17 +0800

    Updated files
    * Added files:
    - resources
    - css
    - databases
    - processors
    
    * Updated index.php codes:
    - added more semantical coding for HTML5
    - added codes for the tab icon, and css include

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
+<!DOCTYPE html>
 <html>
     <header>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="./resources/mainIcon.png">
+        <link rel="stylesheet" href="./css/mainStyle.css">
         <title>HMS</title>
         <!-- insert icon tag here... -->
     </header>
+
     <body>
         <h1>Hello welcome to our Homework Management System!</h1>
-        <a href="register.php">REGISTER HERE!</a>
-        <a href="login.php">LOGIN HERE!</a>
-        
+        <a href="registerPage.php">REGISTER HERE!</a>
+        <a href="loginPage.php">LOGIN HERE!</a>
     </body>
+
     <footer>
         <!-- insert here... -->
     </footer>
