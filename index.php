959fc1ace2dd03801268141aba0126f988e41895
arepoGiovanna <nigelisidto@gmail.com>
2022-07-06 02:28:30 +0800

    Added new CSS Lines
    - Successully learned on how to use the borders to play around.
    
    - Successfully created 2 vertical lines in index.php

--- index.php
+++ index.php
@@ -6,20 +6,24 @@
 <html>
     <header>
         <title>HMS</title>
-        <?php include"./header.php" ?>
+        <?php include"./header.php"; ?>
     </header>
 
     <body>
-        <h1>Dashboard</h1>
-        <div><a href="./registerPage.php">REGISTER</a></div>
-        <div><a href="./loginPage.php">LOGIN</a></div>
-        <div><a href="./homeworkPage.php">HOMEWORK</a></div>
-        <div><a href="./calendarPage.php">CALENDAR</a></div>
-        <div><a href="./buddyPage.php">BUDDY</a></div>
-        <div><a href="./bMark_RefPage.php">BOOKMARKS & REFERENCES</a></div>
+        <div class="firstDiv">
+            <h1>Dashboard</h1>
+            It does nothing
+            <div><a href="./registerPage.php">REGISTER</a></div>
+            <div><a href="./loginPage.php">LOGIN</a></div>
+            <div><a href="./homeworkPage.php">HOMEWORK</a></div>
+            <div><a href="./calendarPage.php">CALENDAR</a></div>
+            <div><a href="./buddyPage.php">BUDDY</a></div>
+            <div><a href="./bMark_RefPage.php">BOOKMARKS & REFERENCES</a></div>
+        </div>
+
     </body>
 
     <footer>
-        <?php include"./footer.php" ?>
+        <?php include"./footer.php"; ?>
     </footer>
 </html>
